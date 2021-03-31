2f795065334d92acd9e23cc86b705c09e1e30c57
alifaragmahmed <alifaragmahmed@gmail.com>
2021-03-31 13:12:47 +0200

    Update awb_price2.blade.php

--- resources/views/reports/awb_price2.blade.php
+++ resources/views/reports/awb_price2.blade.php
@@ -57,7 +57,7 @@
                 <td></td>
                 <td></td>
                 <td>
-                    {{ $finalTotal }}
+                    {{ $totalNet }}
                 </td>
             </tr>
     </table>
